3dbad3eac1da330108477b6cf2405f553a1c9f2b
laders23 <laders23@naver.com>
2023-11-13 21:38:57 +0900

    feat#1 db_ver

--- This/fourtuna/accounts/models.py
+++ This/fourtuna/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    user_id = models.CharField(max_length=20)
+    email  = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    # nickname = models.CharField(max_length=20)
+    # Profile_Image
+    # Gender
+    # Interest_Field
+    # Introduce
+    # Birthday
