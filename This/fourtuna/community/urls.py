e7611723376b981b3f6d5e130bc6f344e9975d7b
b1uesoda <b1uesoda@naver.com>
2023-11-17 18:35:55 +0900

    form#2 Add Community

--- This/fourtuna/community/urls.py
+++ This/fourtuna/community/urls.py
@@ -4,15 +4,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
+app_name = 'community'
 
 urlpatterns = [
     
-    path('', views.community_main),
+    path('board/list/', views.board_list, name='list'),
+    path('board/write/', views.board_write),
+    path('board/detail/<int:pk>', views.board_detail) # int형 pk라는 변수명으로 받음 ex detail/1 (= 첫번째 글)            
   
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
- 
-  # path('mypage/', views.mypage_view),
-  # path('login/', views.login_view),
-  # path('register/', views.register_view),
-  # path('sign', include('account.urls')),
--- This/fourtuna/fourtuna/settings.py
+++ This/fourtuna/fourtuna/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
-    "board",
     "tag",
+    "community",
+    
 ]
 
 MIDDLEWARE = [
