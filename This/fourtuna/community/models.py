24868a83c6dff4e00d88a15006a1f45373d5e0bb
b1uesoda <b1uesoda@naver.com>
2023-12-01 04:10:35 +0900

    feat#2 post_modify, post_delete

--- This/fourtuna/community/models.py
+++ This/fourtuna/community/models.py
@@ -3,15 +3,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
   title = models.CharField(max_length=200)
   content = models.TextField()
   create_date = models.DateTimeField()
+  modify_date = models.DateTimeField(null=True, blank=True)
 
   def __str__(self):
     return self.title
 
 
 class Comment(models.Model):
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
   content = models.TextField()
   create_date = models.DateTimeField()
+  modify_date = models.DateTimeField(null=True, blank=True)
--- This/fourtuna/community/views.py
+++ This/fourtuna/community/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import redirect
 from .forms import PostForm, CommentForm
 from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -18,26 +20,30 @@ def detail(request, post_id):
   context = {'post':post}
   return render(request, 'templates/community/post_detail.html', context)
 
+@login_required(login_url='accounts:login')
 def comment_create(request, post_id):
   post = get_object_or_404(Post, pk=post_id)
   if request.method == "POST":
     form = CommentForm(request.POST)
     if form.is_valid():
       comment = form.save(commit=False)
+      comment.author = request.user
       comment.create_date = timezone.now()
       comment.post = post
       comment.save()
       return redirect('community:detail', post_id=post.id)
   else:
-    return HttpResponseNotAllowed('Only POST is possible.')
+    form = CommentForm()
   context = {'post': post, 'form': form}
   return render(request, 'templates/community/post_detail.html', context)
 
+@login_required(login_url='accounts:login')
 def post_create(request):
   if request.method == "POST":
     form = PostForm(request.POST)
     if form.is_valid():
       post = form.save(commit=False)
+      post.author = request.user
       post.create_date = timezone.now()
       post.save()
       return redirect('community:index')
@@ -46,3 +52,29 @@ def post_create(request):
   context = {'form': form}
   return render(request, 'templates/community/post_form.html', context)
   
+@login_required(login_url='accounts:login')
+def post_modify(request, post_id):
+  post = get_object_or_404(Post, pk=post_id)
+  if request.user != post.author:
+    messages.error(request, '수정 권한이 없습니다')
+    return redirect('community:detail', post_id=post.id)
+  if request.method == "POST":
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.modify_date = timezone.now()
+      post.save()
+      return redirect('community:detail', post_id=post.id)
+  else:
+    form = PostForm(instance=post)
+  context = {'form': form}
+  return render(request, 'templates/community/post_form.html', context)
+
+@login_required(login_url='accounts:login')
+def post_delete(request, post_id):
+  post = get_object_or_404(Post, pk=post_id)
+  if request.user != post.author:
+    messages.error(request, "삭제 권한이 없습니다")
+    return redirect('community:detail', post_id=post.id)
+  post.delete()
+  return redirect('community:index')
