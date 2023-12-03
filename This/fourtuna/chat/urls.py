30244b71b7773ae97d083fb8b2a24b2353252a75
b1uesoda <b1uesoda@naver.com>
2023-12-04 02:53:47 +0900

    feat#2 api

--- This/fourtuna/chat/urls.py
+++ This/fourtuna/chat/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 from .views import index, room, create_room
 
 app_name = 'chat'
@@ -6,6 +6,6 @@ app_name = 'chat'
 urlpatterns = [
     path('', index, name='index'),
     path('create/', create_room, name='create_room'),
-    re_path(r'^(?P<room_id>[^/]+)/$', room, name='room'),
+    path('<int:room_id>/', room, name='room'),
 
 ]
