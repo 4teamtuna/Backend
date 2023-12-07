56f75074854e9262751d050573672366d0d1c7d4
b1uesoda <b1uesoda@naver.com>
2023-12-07 21:43:02 +0900

    feat#2

--- This/fourtuna/fourtuna/urls.py
+++ This/fourtuna/fourtuna/urls.py
@@ -1,22 +1,5 @@
-"""
-URL configuration for fourtuna project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf.urls import include
-
 from django.urls import path
 from django.views.generic import RedirectView
 from django.conf import settings
@@ -26,23 +9,7 @@ from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view 
 from drf_yasg import openapi
 
-schema_url_patterns = [ 
-    path('', include('users.urls')),
-    ]
 
-schema_view_v1 = get_schema_view(
-    openapi.Info(
-        title="Open API",
-        default_version='v1',
-        description="시스템 API",
-        terms_of_service="https://www.google.com/policies/terms/",
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
-    patterns=schema_url_patterns,
-)
-
-# from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -50,9 +17,4 @@ urlpatterns = [
     path('community/', include('community.urls')),
     path('', include('users.urls')),
     path('team/', include('team.urls', namespace='team')),
-
-
-    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view_v1.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'^swagger/$', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'^redoc/$', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
