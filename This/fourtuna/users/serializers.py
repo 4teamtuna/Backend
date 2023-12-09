7671ebb7af2bd4f4d2de062da9314107be12eebe
baekggum <c3man4@naver.com>
2023-12-10 01:16:07 +0900

    hm

--- This/fourtuna/users/serializers.py
+++ This/fourtuna/users/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import UserProfile
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['introduction', 'interest_field', 'birth_date', 'profile_image']
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'profile']
