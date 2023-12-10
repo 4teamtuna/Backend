1962fcf2fe003e41e3eb94cf3f6f7eeb20c80e0f
laders23 <laders23@naver.com>
2023-12-11 06:08:10 +0900

    try:8 users.serializer modify

--- This/fourtuna/users/serializers.py
+++ This/fourtuna/users/serializers.py
@@ -2,8 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import UserProfile
 
-<<<<<<< HEAD
-class UserProfileSerializer(serializers.ModelSerializer):
+'''class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['introduction', 'interest_field', 'birth_date', 'profile_image']
@@ -13,7 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'profile']
-=======
+        
+'''
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -23,4 +24,4 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['introduction', 'interest_field', 'birth_date', 'profile_image', 'nickname']
->>>>>>> 6c79ddd56d29146e592a27eac616360e5bfc2086
+
