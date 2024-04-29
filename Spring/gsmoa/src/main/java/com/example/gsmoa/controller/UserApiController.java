bc502862093e8f781a6de7ad965d205e9f9d20b1
laders23 <laders23@naver.com>
2024-04-29 22:08:18 +0900

    UserAPI fix

--- Spring/gsmoa/src/main/java/com/example/gsmoa/controller/UserApiController.java
+++ Spring/gsmoa/src/main/java/com/example/gsmoa/controller/UserApiController.java
@@ -5,6 +5,9 @@ import com.example.gsmoa.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
@@ -16,11 +19,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserApiController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/user")
     public String signup(AddUserRequest request){
         userService.save(request); // 회원 가입 메소드 호출
         return "redirect:/login"; // 회원 가입이 완료된 후 로그인 페이지로 이동
+    }
+
+    @PostMapping("/login")
+    public String login(String email, String password){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "redirect:/articles"; // 로그인이 완료된 후 기사 페이지로 이동
     }
 
     @GetMapping("/logout")
