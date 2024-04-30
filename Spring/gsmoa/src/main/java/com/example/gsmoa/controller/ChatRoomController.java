2c6282ebffa544a87aab6e78f9bd789d714249e1
b1uesoda <b1uesoda@naver.com>
2024-05-01 02:54:35 +0900

    feat#2 fix

--- Spring/gsmoa/src/main/java/com/example/gsmoa/controller/ChatRoomController.java
+++ Spring/gsmoa/src/main/java/com/example/gsmoa/controller/ChatRoomController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-@Controller
+@RestController
 @Slf4j
 public class ChatRoomController {
 
