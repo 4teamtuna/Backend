1fcf55d270c33e7bfaa074ec2cdd6dd1788510f7
b1uesoda <b1uesoda@naver.com>
2024-05-02 16:36:18 +0900

    feat#2 restart

---
+++ gsmoa/src/test/java/com/example/gsmoa/GsmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
