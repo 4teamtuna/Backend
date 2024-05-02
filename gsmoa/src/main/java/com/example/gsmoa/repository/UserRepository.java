a21c2ecec96fa89fbb11103c7c2a62402bd953eb
b1uesoda <b1uesoda@naver.com>
2024-05-03 00:21:00 +0900

    link db and create entity

---
+++ gsmoa/src/main/java/com/example/gsmoa/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.gsmoa.repository;
+
+import com.example.gsmoa.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+
+}
