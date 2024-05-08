2ddd5f28b280a21a9c9be7123d31fe4c9bb4abb3
laders23 <laders23@naver.com>
2024-05-08 14:15:50 +0900

    MSA_Ver1 업데이트

---
+++ MSA_ver1/gsmoa-msa/DetailServer/src/main/java/com/detailserver/DetailServerApplication.java
@@ -0,0 +1,13 @@
+package com.detailserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetailServerApplication.class, args);
+    }
+
+}
---
+++ MSA_ver1/gsmoa-msa/src/main/java/com/gsmoamsa/GsmoaMsaApplication.java
@@ -0,0 +1,13 @@
+package com.gsmoamsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmoaMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsmoaMsaApplication.class, args);
+	}
+
+}
