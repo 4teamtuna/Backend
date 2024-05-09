9844aa41ccfed2367cd3752a8c65aae8b3d76845
laders23 <laders23@naver.com>
2024-05-09 23:22:36 +0900

    MSA_ver3

---
+++ MSA_ver3/MSA_ver3/src/main/java/com/example/msa_ver3/MsaVer3Application.java
@@ -0,0 +1,13 @@
+package com.example.msa_ver3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaVer3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaVer3Application.class, args);
+    }
+
+}
