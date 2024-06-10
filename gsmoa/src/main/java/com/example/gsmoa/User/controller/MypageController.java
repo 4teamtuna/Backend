3be812f4d9b2fc8786c634322b2aa8c58f9a6409
b1uesoda <b1uesoda@naver.com>
2024-06-11 00:09:17 +0900

    modify update introduce api

--- gsmoa/src/main/java/com/example/gsmoa/User/controller/MypageController.java
+++ gsmoa/src/main/java/com/example/gsmoa/User/controller/MypageController.java
@@ -1,8 +1,12 @@
 package com.example.gsmoa.User.controller;
 
+import com.example.gsmoa.User.dto.UpdateIntroduce;
 import com.example.gsmoa.User.dto.UserDto;
 import com.example.gsmoa.User.dto.UserUpdateDto;
+import com.example.gsmoa.User.entity.UserEntity;
+import com.example.gsmoa.User.repository.UserRepository;
 import com.example.gsmoa.User.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +15,12 @@ import org.springframework.web.bind.annotation.*;
 public class MypageController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public MypageController(UserService userService) {
+    public MypageController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/main/my")
@@ -27,5 +33,10 @@ public class MypageController {
     public UserDto updateMyInfo(@RequestBody UserUpdateDto userUpdateDto) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         return userService.updateUser(username, userUpdateDto);
+    }
+
+    @PutMapping("/main/update/introduce/{userId}")
+    public UserEntity updateIntroduce(@RequestBody String newIntroduce, @PathVariable Integer userId) {
+        return userService.updateUserIntroduce(userId, newIntroduce);
     }
 }
---
+++ gsmoa/src/main/java/com/example/gsmoa/User/dto/UpdateIntroduce.java
@@ -0,0 +1,10 @@
+package com.example.gsmoa.User.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateIntroduce {
+    String introduce;
+}
